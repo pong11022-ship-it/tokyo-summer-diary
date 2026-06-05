561e37ef88760d3e8f7fed56461ad4ba4d1047df
pong11022-ship-it <pong11022@gmail.com>
2026-06-05 22:53:43 +0900

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-const swiper=new Swiper('.diarySwiper',{speed:600});
-const c=document.querySelector('.counter');
-const total=document.querySelectorAll('.swiper-slide').length;
-function u(){c.textContent=`${String(swiper.activeIndex+1).padStart(2,'0')} / ${String(total).padStart(2,'0')}`}
-u();swiper.on('slideChange',u);
